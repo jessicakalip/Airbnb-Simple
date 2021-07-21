c3939c9057de763a371b252a134a749ac5443acb
jessica kalip <jessicakalip@gmail.com>
2021-07-21 17:03:33 +0800

    Completed without delete function

---
+++ app/controllers/flats_controller.rb
@@ -0,0 +1,36 @@
+class FlatsController < ApplicationController
+  before_action :find_flat, only: %i[show edit update]
+
+  def index
+    @flats = Flat.all
+  end
+
+  def show; end
+
+  def new
+    @flat = Flat.new
+  end
+
+  def create
+    @flat = Flat.new(flat_params)
+    @flat.save
+    redirect_to flats_path
+  end
+
+  def edit; end
+
+  def update
+    @flat.update(flat_params)
+    redirect_to flats_path(@flat)
+  end
+
+  private
+
+  def find_flat
+    @flat = Flat.find(params[:id])
+  end
+
+  def flat_params
+    params.require(:flat).permit(:name, :address, :description, :price_per_night, :number_of_guests)
+  end
+end
